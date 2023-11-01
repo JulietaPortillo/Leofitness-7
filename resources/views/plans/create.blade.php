30dffa5648dc0895f82979b4f5f7255a85233cff
Julieta Portillo <julie.portillo96@gmail.com>
2023-11-01 01:04:33 -0400

    Migrar planes

---
+++ resources/views/plans/create.blade.php
@@ -0,0 +1,44 @@
+@extends('app')
+
+@section('content')
+
+    <div class="rightside bg-grey-100">
+        <div class="container-fluid">
+
+            <!-- Error Log -->
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <strong>Whoops!</strong> Hubo problemas con los datos ingresados.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel no-border">
+                        <div class="panel-title">
+                            <div class="panel-head font-size-20">Ingrese detalles del plan.</div>
+                        </div>
+
+                        {!! Form::Open(['url' => 'plans','id'=>'plansform']) !!}
+
+                        @include('plans.form',['submitButtonText' => 'Agregar plan'])
+
+                        {!! Form::Close() !!}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@stop
+@section('footer_scripts')
+    <script src="{{ URL::asset('assets/js/plan.js') }}" type="text/javascript"></script>
+@stop
---
+++ resources/views/plans/form.blade.php
@@ -0,0 +1,80 @@
+<div class="panel-body">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                {!! Form::label('plan_code','Codigo del plan') !!}
+                {!! Form::text('plan_code',null,['class'=>'form-control', 'id' => 'plan_code']) !!}
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                {!! Form::label('plan_name','Nombre del plan') !!}
+                {!! Form::text('plan_name',null,['class'=>'form-control', 'id' => 'plan_name']) !!}
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                {!! Form::label('plan_details','Detalles del plan') !!}
+                {!! Form::text('plan_details',null,['class'=>'form-control', 'id' => 'plan_details']) !!}
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                <?php $services = App\Service::lists('name', 'id'); ?>
+                {!! Form::label('service_id','Service') !!}
+                {!! Form::select('service_id',$services,null,['class'=>'form-control selectpicker show-tick show-menu-arrow','id'=>'service_id','data-live-search'=> 'true']) !!}
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                {!! Form::label('days','Dias') !!}
+                {!! Form::text('days',null,['class'=>'form-control', 'id' => 'days']) !!}
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                {!! Form::label('amount','Cantidad (sin cargos adicionales)') !!}
+                <div class="input-group">
+                    <div class="input-group-addon"><i class="fa fa-money"></i></div>
+                    {!! Form::text('amount',null,['class'=>'form-control', 'id' => 'amount']) !!}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+            {!! Form::label('status','Status') !!}
+            <!--0 for inactive , 1 for active-->
+                {!! Form::select('status',array('1' => 'Activo', '0' => 'InActivo'),null,['class' => 'form-control selectpicker show-tick show-menu-arrow', 'id' => 'status']) !!}
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary pull-right']) !!}
+            </div>
+        </div>
+    </div>
+</div>
+                            
